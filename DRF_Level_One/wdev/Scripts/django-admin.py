f79b34a26310f6e2a86a1111c692a2c086877c61
Arturo Perez <prz.arturo@gmail.com>
2020-04-11 22:09:50 -0700

    Add VirtualEnv for Windows

---
+++ DRF_Level_One/wdev/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\potte\pycharmprojects\drf_course\drf_level_one\wdev\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
